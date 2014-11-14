6b66727c361e68e115325a55a3cca71c0a05583f
Mark Clarkson <mark.clarkson@smorg.co.uk>
2014-11-14 10:36:21 +0000

    Don't do a secret pull from cache

--- static/plugins/saltconfigserver/scripts/salt-grains.sh
+++ static/plugins/saltconfigserver/scripts/salt-grains.sh
@@ -28,31 +28,10 @@ salt_id=$1
 output=`salt --output=json "$salt_id" grains.items`
 
 [[ -z $output ]] && {
-
-output=`python < <(cat <<EnD
-#!/usr/bin/env python
-import json
-import os
-import sys
-import getopt
-
-import salt.config
-import salt.runner
-
-if __name__ == '__main__':
-    __opts__ = salt.config.master_config(
-            os.environ.get('SALT_MASTER_CONFIG', '/etc/salt/minion'))
-    runner = salt.runner.Runner(__opts__)
-
-    stdout_bak = sys.stdout
-    with open(os.devnull, 'wb') as f:
-        sys.stdout = f
-        items = runner.cmd("cache.grains", ["$salt_id"])
-    sys.stdout = stdout_bak
-
-    print json.dumps(items)
-EnD
-)`
+    echo '{"Error":"Salt query returned no output."}'
+    exit 1
 }
 
 echo $output
+
+exit 0
