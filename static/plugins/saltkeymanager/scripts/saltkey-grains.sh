a406b51dc3ce7423afef87cf240b0f91b5403610
Mark Clarkson <mark.clarkson@smorg.co.uk>
2014-12-30 10:17:54 +0000

    Don't mask the error by getting data from the cache

--- static/plugins/saltkeymanager/scripts/saltkey-grains.sh
+++ static/plugins/saltkeymanager/scripts/saltkey-grains.sh
@@ -27,32 +27,43 @@ salt_id=$1
 
 output=`salt --output=json "$salt_id" grains.items`
 
+# -- don't pull from cache, it masks the failure
+# 
+# [[ -z $output ]] && {
+# 
+# output=`python < <(cat <<EnD
+# #!/usr/bin/env python
+# import json
+# import os
+# import sys
+# import getopt
+# 
+# import salt.config
+# import salt.runner
+# 
+# if __name__ == '__main__':
+#     __opts__ = salt.config.master_config(
+#             os.environ.get('SALT_MASTER_CONFIG', '/etc/salt/minion'))
+#     runner = salt.runner.Runner(__opts__)
+# 
+#     stdout_bak = sys.stdout
+#     with open(os.devnull, 'wb') as f:
+#         sys.stdout = f
+#         items = runner.cmd("cache.grains", ["$salt_id"])
+#     sys.stdout = stdout_bak
+# 
+#     print json.dumps(items)
+# EnD
+# )`
+# }
+
+q="'"
+
 [[ -z $output ]] && {
-
-output=`python < <(cat <<EnD
-#!/usr/bin/env python
-import json
-import os
-import sys
-import getopt
-
-import salt.config
-import salt.runner
-
-if __name__ == '__main__':
-    __opts__ = salt.config.master_config(
-            os.environ.get('SALT_MASTER_CONFIG', '/etc/salt/minion'))
-    runner = salt.runner.Runner(__opts__)
-
-    stdout_bak = sys.stdout
-    with open(os.devnull, 'wb') as f:
-        sys.stdout = f
-        items = runner.cmd("cache.grains", ["$salt_id"])
-    sys.stdout = stdout_bak
-
-    print json.dumps(items)
-EnD
-)`
+    echo '{"Error":"No output from '${q}'salt --output=json "'"$salt_id"'" grains.items'${q}
+    echo ' Check that the server is up and the salt-minion is running.'
+    exit 1
 }
 
 echo $output
+exit 0
