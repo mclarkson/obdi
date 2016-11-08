fa0d28b978d22e593c9e210c3d31a72042bf1140
Mark Clarkson <mark.clarkson@smorg.co.uk>
2016-11-08 09:48:31 +0000

    Use frags/ dir

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -1,5 +1,5 @@
 # Run the one-liner from base dir
 
-git checkout master; pushd doc; for i in *.md; do markdown $i >/var/tmp/${i%.md}.frag; done; popd; git checkout gh-pages; mv /var/tmp/*.frag .; for i in *.frag; do cat header.frag $i footer.frag >${i%.frag}.html; done
+git checkout master; pushd doc; for i in *.md; do markdown $i >/var/tmp/${i%.md}.frag; done; popd; git checkout gh-pages; mv /var/tmp/*.frag .; for i in *.frag; do cat frags/header.frag $i frags/footer.frag >${i%.frag}.html; done
 
 # Then git commit push etc
