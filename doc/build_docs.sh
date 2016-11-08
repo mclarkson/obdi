6afa6abc3dbd88fa78f58f65726b721b43672bff
Mark Clarkson <mark.clarkson@smorg.co.uk>
2016-11-08 10:04:39 +0000

    Fleshing out

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -1,5 +1,25 @@
-# Run the one-liner from base dir
+# Run this file from base dir, e.g.
+#
+#   bash ./doc/build_docs.sh
 
-git checkout master; pushd doc; for i in *.md; do markdown $i >/var/tmp/${i%.md}.frag; done; popd; git checkout gh-pages; mv /var/tmp/*.frag .; for i in *.frag; do cat frags/header.frag $i frags/footer.frag >${i%.frag}.html; done
+git checkout master
+pushd doc
+tmpdir=$(mktemp -d)
+for i in *.md; do
+    markdown $i >$tmpdir/${i%.md}.frag
+done
+popd
+
+git checkout gh-pages
+mv /var/tmp/*.frag .
+sed "s/{{NAME}}/$NAME/;s/{{TAGLINE}}/$TAGLINE/" frags/header.frag >$tmpdir/header
+for i in $tmpdir/*.frag; do
+    cat $tmpdir/header $i frags/footer.frag >${i%.frag}.html
+done
+
+rm -f *.frag
+rm -f header
+rm -f $tmpdir/*
+rmdir $tmpdir
 
 # Then git commit push etc
