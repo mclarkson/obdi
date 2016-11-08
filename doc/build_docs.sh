52b28fa71619aaee5c3f07595ef49d6bf21e00ef
Mark Clarkson <mark.clarkson@smorg.co.uk>
2016-11-08 10:10:42 +0000

    Fix

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -11,7 +11,6 @@ done
 popd
 
 git checkout gh-pages
-mv $tmpdir/*.frag .
 sed "s/{{NAME}}/$NAME/;s/{{TAGLINE}}/$TAGLINE/" frags/header.frag >$tmpdir/header
 for i in $tmpdir/*.frag; do
     cat $tmpdir/header $i frags/footer.frag >${i%.frag}.html
