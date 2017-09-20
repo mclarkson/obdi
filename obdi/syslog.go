92ea30e053a98fefa9d8c09119ea56cfdaea121e
Mark Clarkson <mark.clarkson@smorg.co.uk>
2017-09-20 12:04:17 +0100

    Switch for syslog

--- obdi/syslog.go
+++ obdi/syslog.go
@@ -20,12 +20,16 @@ import (
 )
 
 func logit(msg string) {
-	log.Println(msg)
-	l, err := syslog.New(syslog.LOG_ERR, "obdi")
-	defer l.Close()
-	if err != nil {
-		log.Fatal("error writing syslog!")
-	}
 
-	l.Err(msg)
+	log.Println(msg)
+
+	if config.SyslogEnabled {
+		l, err := syslog.New(syslog.LOG_ERR, "obdi")
+		defer l.Close()
+		if err != nil {
+			log.Fatal("error writing syslog!")
+		}
+
+		l.Err(msg)
+	}
 }
